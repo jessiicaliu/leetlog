2c028946c20425039324d9498700d7f15b946798
Jessica Liu <j2357liu@uwaterloo.ca>
2026-04-20 11:51:57 -0400

    only allow popup on leetcode pages

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+// Only enable the popup on LeetCode problem pages
+chrome.action.disable();
+
+function updateAction(tab) {
+  if (tab.url?.includes("leetcode.com/problems/")) {
+    chrome.action.enable(tab.id);
+  } else {
+    chrome.action.disable(tab.id);
+  }
+}
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs.get(tabId, updateAction);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") updateAction(tab);
+});
+
 chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg.type === "LEETCODE_TAB_OPEN") {
     chrome.storage.local.set({ leetcodeTabId: sender.tab.id });
